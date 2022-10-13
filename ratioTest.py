ab753449f5054a1f0ab926b3dd3966ea0273dca5
Faucona <Faucona@users.noreply.github.com>
2022-10-12 22:31:06 -0400

    final1
    - je vais faire une revision demain mais  on devrait pas mal avoir tout pour analyser

--- ratioTest.py
+++ ratioTest.py
@@ -2,13 +2,14 @@ import csv
 import math
 import os
 import sys
-
+import lcsec
 def ratioTest(path):
 	for file in os.listdir(path):
 
 		result = os.path.join(path, file)
 		if(file.endswith('.java')):
 			tabTotal.append(file)
+
 			if ("Test" in file):
 				for i in range(len(tabValue)):
 					if(file.replace("Test","") in tabValue[i][0]):
@@ -17,9 +18,14 @@ def ratioTest(path):
 						break
 
 			else :
+				#print(file)
+				#varf = file.replace('.java','')
+				#print(varf)
 				strResult = f"{result}, {os.path.getsize(os.path.join(path, file))}"
-				tabResult = strResult.split(',') 
+				tabResult = strResult.split(',')
+				tabResult.append(file.replace('.java','')) 
 				tabValue.append(tabResult)
+				
 
 		elif os.path.isdir(result) :
 			ratioTest(result)
@@ -40,10 +46,18 @@ def result(string):
 	tab = ratioTest(start(string))
 
 	moyenne = 0
+	ecartType = 0
 	for value in tabTestValue : 
+		#print(value)
 		moyenne += value[-1]
 
 	moyenne = round(moyenne/len(tabTestValue),4)
+
+	for value in tabTestValue : 
+		ecartType += (moyenne - value[-1])**2
+
+	ecartType = math.sqrt(ecartType/len(tabTestValue))
+
 
 	with open('classeTester.csv', 'w', newline='') as myfile:
 	    wr = csv.writer(myfile, delimiter=',')
@@ -53,13 +67,30 @@ def result(string):
 	    wr = csv.writer(myfile, delimiter=',')
 	    wr.writerows(tabValue)
 
+	#for value in tabValue :
+	
+	tab3 = lcsec.result(string,'classeNonTester.csv')
+
+	moyenne2 = 0
+	#ecartType = 0
+	for value in tab3 : 
+		moyenne2 += value[-1]
+
+	moyenne2 = round(moyenne2/len(tab3),4)
+	
+	with open('classeNonTesterCBO.csv', 'w', newline='') as myfile:
+	    wr = csv.writer(myfile, delimiter=',')
+	    wr.writerows(tab3)
+
 
 	print(f"nombre de classe total : {len(tabTotal)}")
 	print(f"nombre de classe tester : {len(tabTestValue)}")
 	print(f"nombre de classe non tester : {len(tabValue)}")
 	print(f"nombre de classeTest non associé à une classe repertorié : {len(tabTotal) - len(tabValue) - len(tabTestValue)*2}")
 	print(f"pourcentage de classe non tester : {round(len(tabValue)/len(tabTotal),4)*100}%" )
+	print(f"en moyenne les fichier non tester on un CBO de : {round(moyenne2,4)}" )
 	print(f"en moyenne la taille fichier d'un fichier test est : {moyenne*100}% de son fichier tester" )
+	print(f"ecart type de la taille d'un fichier test est : {round(ecartType,4)}" )
 
 if __name__ == "__main__":
 	result(sys.argv[1]) 
--- start.py
+++ start.py
@@ -5,6 +5,7 @@ import lcsec
 import os
 import sys
 import csv
+import math
 
 
 ratioTest.result(sys.argv[1])
@@ -45,22 +46,160 @@ def documentation(path):
 	moyenneCloc = moyenneCloc/len(tabCloc)
 	print(f"en moyenne les fichiers ont une densité de commentaire de  : {round(moyenneCloc,4)}" )
 
+def CBOSort(tab):
+	return int(tab[-1])
+
+def complexitySort(tab):
+	return int(tab[-2])
+
 def complexity(path):
 	complexityMcCabe(path)
+	#q1(tabTotalFile)
+
+
+
+
+
+def q1 (tabTotalFile):
+	tab1 = sorted(tabTotalFile,key=CBOSort, reverse=True)
+	threshold10 = math.ceil(10/100 *len(tab1))
+	threshold30 = math.ceil(30/100 *len(tab1))
+	#tab30 = []
+	threshold60 = math.ceil(60/100 *len(tab1))
+	#tab60 = []
+	threshold90 = math.ceil(90/100 *len(tab1))
+	#tab90 = []
+
+
+	for value in tab1:
+		value.append(cloc.result(value[0]))
+
+
+	moyenneDensite10 = 0
+	moyenneDensite30 = 0
+	moyenneDensite60 = 0
+	moyenneDensite90 = 0
 	moyenneMcCabe = 0 
-	for value in tabTotalFile:
-		moyenneMcCabe += value[-1]
-	moyenneMcCabe = moyenneMcCabe/len(tabTotalFile)
+
+	for i in range(threshold90):
+		if(i < threshold10):
+			moyenneDensite10 += tab1[i][-1]
+		if(i < threshold30):
+			moyenneDensite30 += tab1[i][-1]
+		if(i < threshold60):
+			moyenneDensite60 += tab1[i][-1]
+		moyenneDensite90 += tab1[i][-1]
+
+	moyenneDensite10 = moyenneDensite10/threshold10
+	moyenneDensite30 = moyenneDensite30/threshold30
+	moyenneDensite60 = moyenneDensite60/threshold60
+	moyenneDensite90 = moyenneDensite90/threshold90
+
+	for value in tab1:
+		moyenneMcCabe += value[-2]
+	moyenneMcCabe = moyenneMcCabe/len(tab1)
 
 	with open('mcCabe.csv', 'w', newline='') as myfile:
 	     wr = csv.writer(myfile, delimiter=',')
-	     wr.writerows(tabTotalFile)
+	     wr.writerows(tab1)
+
+	with open('mcCabe10.csv', 'w', newline='') as myfile:
+	     wr = csv.writer(myfile, delimiter=',')
+	     wr.writerows(tab1[:threshold10])
+
+	with open('mcCabe30.csv', 'w', newline='') as myfile:
+	     wr = csv.writer(myfile, delimiter=',')
+	     wr.writerows(tab1[:threshold30])
+
+	with open('mcCabe60.csv', 'w', newline='') as myfile:
+	     wr = csv.writer(myfile, delimiter=',')
+	     wr.writerows(tab1[:threshold60])
+
+	with open('mcCabe90.csv', 'w', newline='') as myfile:
+	     wr = csv.writer(myfile, delimiter=',')
+	     wr.writerows(tab1[:threshold90])
+
 	print(f"en moyenne les fichiers ont une complexity de  : {round(moyenneMcCabe,4)}" )
+	print(f"à un seuil de 10% les fichiers ont en moyenne une densité de  : {round(moyenneDensite10,4)}" )
+	print(f"à un seuil de 30% les fichiers ont en moyenne une densité de  : {round(moyenneDensite30,4)}" )
+	print(f"à un seuil  60% les fichiers ont en moyenne une densité de  : {round(moyenneDensite60,4)}" )
+	print(f"à un seuil  90% les fichiers ont en moyenne une densité de  : {round(moyenneDensite90,4)}" )
+
+
+
+def q2():
+	tab = couple(sys.argv[1])
+	threshold10 = math.ceil(10/100 *len(tab))
+	threshold15 = math.ceil(15/100 *len(tab))
+	tab1 = sorted(tab,key=CBOSort,reverse=True)
+	tab2 = sorted(tab,key=complexitySort,reverse=True)
+	temptab1 = []
+	temptab2 = []
+	moyenne10Complexity = 0 
+	moyenne15Complexity = 0
+	moyenne10CBO = 0 
+	moyenne15CBO = 0
+
+	with open('CBO10.csv', 'w', newline='') as myfile:
+	     wr = csv.writer(myfile, delimiter=',')
+	     wr.writerows(tab1[:threshold10])
+
+	with open('CBO15.csv', 'w', newline='') as myfile:
+	     wr = csv.writer(myfile, delimiter=',')
+	     wr.writerows(tab1[:threshold15])
+
+	with open('complexity10.csv', 'w', newline='') as myfile:
+	     wr = csv.writer(myfile, delimiter=',')
+	     wr.writerows(tab2[:threshold10])
+
+	with open('complexity15.csv', 'w', newline='') as myfile:
+	     wr = csv.writer(myfile, delimiter=',')
+	     wr.writerows(tab2[:threshold15])
+
+
+	for i in range(threshold10):
+		for j in range(threshold10):
+			if(tab1[i][0] == tab2[j][0]):
+				temptab1.append(tab1[i])
+				moyenne10Complexity += int(tab1[i][-2])
+				moyenne10CBO += int(tab1[i][-1])
+				break
+	for i in range(threshold15):
+		for j in range(threshold15):
+			if(tab1[i][0] == tab2[j][0]):
+				temptab2.append(tab1[i])
+				moyenne15Complexity += int(tab1[i][-2])
+				moyenne15CBO += int(tab1[i][-1])
+				break
 	
+	
+		
+
+	moyenne10Complexity = moyenne10Complexity/len(temptab1)
+	moyenne15Complexity = moyenne15Complexity/len(temptab2)
+	moyenne10CBO = moyenne10CBO/len(temptab1)
+	moyenne15CBO = moyenne15CBO/len(temptab2)
+
+	with open('CBO_WMC10.csv', 'w', newline='') as myfile:
+	     wr = csv.writer(myfile, delimiter=',')
+	     wr.writerows(temptab1)
+
+	with open('CBO_WMC15.csv', 'w', newline='') as myfile:
+	     wr = csv.writer(myfile, delimiter=',')
+	     wr.writerows(temptab2)
+
+	#print(f"en moyenne les fichiers ont une complexity de  : {round(moyenneMcCabe,4)}" )
+	print(f"à un seuil de 10% les fichiers ont une complexite de  : {round(moyenne10Complexity,4)}" )
+	print(f"à un seuil de 15% les fichiers ont une complexite de  : {round(moyenne15Complexity,4)}" )
+	print(f"à un seuil  10% les fichiers ont un CBO de  : {round(moyenne10CBO,4)}" )
+	print(f"à un seuil  15% les fichiers ont un CBO de  : {round(moyenne15CBO,4)}" )
+
+	 		
 
 
 def couple(path):
-	lcsec.result(path,"mcCabe.csv")
+	return lcsec.result(path,"mcCabe.csv")
 complexity(sys.argv[1])
 documentation(sys.argv[1])
-couple(sys.argv[1])
+q1(tabTotalFile)
+q2()
